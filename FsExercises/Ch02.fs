1b3ecd89b56df3e2311a768fb5a0947e3d893b2a
Valeriy Reutskiy <vvreutskiy@gmail.com>
2015-07-02 14:56:29 +0300

    Continue working on chapter 2

--- FsExercises/Ch02.fs
+++ FsExercises/Ch02.fs
@@ -14,9 +14,41 @@ module Ch02 =
             match times with
             | 0 -> acc
             | n -> conc s (times - 1) (acc + s)
-        conc (fst v) (snd v) ""    
-    //String.replicate (snd v) (fst v)
+        conc (fst v) (snd v) ""
     
     let e03 (v : string * int * char) : bool = 
         let s, i, c = v
         s.[i] = c
+    
+    let e04 (v : string * int * char) : int = 
+        let (s, i, c) = v
+        let isChar e a = e = a
+        
+        let rec loop (str : string) (acc : int) (chr : char) = 
+            match str with
+            | "" -> acc
+            | s when s.[0] = chr -> loop (s.[1..]) (acc + 1) chr
+            | _ -> loop str.[1..] acc chr
+        if i > s.Length then 0
+        else loop (s.[i..]) 0 c
+    
+    let e05 (v : string * char) = 
+        let s, c = v
+        e04 (s, 0, c)
+    
+    let e06 (v : int * int) = 
+        let d, n = v
+        match n % d with
+        | 0 -> false
+        | _ -> true
+    
+    let e0701 (v : int * int * int) : bool = 
+        let a, b, c = v
+        if a > b then failwith "Argument exception"
+        [ a..b ] |> List.fold (fun acc el -> acc && e06 (el, c)) true
+    
+    let e0702 (n : int) : bool = e0701 (2, n - 1, n)
+    
+    let rec e0703 (n : int) : int = 
+        if e0702 (n + 1) then n + 1
+        else e0703 (n + 1)
--- FsExercisesTests/ch02tests.fs
+++ FsExercisesTests/ch02tests.fs
@@ -22,8 +22,33 @@ module Ch02tests =
     let ``e03 test``() = 
         test <@ e03 ("123", 1, '2') = true @>
         test <@ e03 ("123", 1, 'z') = false @>
-
+    
     [<Fact>]
     let ``e04 test``() = 
-        test <@ false @>
+        test <@ e04 ("123123123", 3, '1') = 2 @>
+        test <@ e04 ("123123123", 20, '1') = 0 @>
+    
+    [<Fact>]
+    let ``e05 test``() = 
+        test <@ e05 ("123123123", '1') = 3 @>
+        test <@ e05 ("123123123", '0') = 0 @>
+    
+    [<Fact>]
+    let ``e06 test``() = 
+        test <@ e06 (3, 4) = true @>
+        test <@ e06 (3, 6) = false @>
+    
+    [<Fact>]
+    let ``e0701 test``() = 
+        test <@ e0701 (3, 5, 7) = true @>
+        test <@ e0701 (3, 5, 8) = false @>
 
+    [<Fact>]
+    let ``e0702 test``() = 
+        test <@ e0702 11 = true @>
+        test <@ e0702 12 = false @>
+
+    [<Fact>]
+    let ``e0703 test``() =
+        test <@ e0703 10 = 11 @>
+        test <@ e0703 11 = 13 @>
